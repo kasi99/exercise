fb7140bd5ac0973f122256461f99441360df64a2
kasi <ht333333@sina.com>
2018-05-27 05:14:07 +0800

    第一次提交

---
+++ git/src/java/com/kasi/exercise/GitExercise.java
@@ -0,0 +1,15 @@
+package com.kasi.exercise;
+
+/**
+ * 练习git命令撤销
+ *
+ * @Author: HuangTao
+ * @Date: 2018-05-27 04:49
+ * @Version:
+ */
+
+public class GitExercise {
+    public static void main(String[] args) {
+        System.out.println("git test...1");
+    }
+}
