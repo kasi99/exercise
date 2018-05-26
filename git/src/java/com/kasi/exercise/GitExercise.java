8a1bc0de9b5f4e19106bafbde200954611672d36
kasi <ht333333@sina.com>
2018-05-27 05:48:56 +0800

    22222

--- git/src/java/com/kasi/exercise/GitExercise.java
+++ git/src/java/com/kasi/exercise/GitExercise.java
@@ -11,5 +11,6 @@ package com.kasi.exercise;
 public class GitExercise {
     public static void main(String[] args) {
         System.out.println("git test...1");
+        System.out.println("git test...2");
     }
 }
