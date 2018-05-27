49a16271208b1e2ba713febb3ca3314bd0aea3cb
kasi <ht333333@sina.com>
2018-05-27 14:46:02 +0800

    第三次提交

--- git/src/java/com/kasi/exercise/GitDemo3.java
+++ git/src/java/com/kasi/exercise/GitDemo3.java
@@ -11,6 +11,5 @@ package com.kasi.exercise;
 public class GitDemo3 {
     public static void main(String[] args) {
         System.out.println("第一次提交");
-        System.out.println("第一次提交");
     }
 }
