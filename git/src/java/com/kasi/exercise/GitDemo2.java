1457eb583a6ee4edd3ae9ed183801b37756f3ac6
kasi <ht333333@sina.com>
2018-05-27 14:11:06 +0800

    第四次提交

--- git/src/java/com/kasi/exercise/GitDemo2.java
+++ git/src/java/com/kasi/exercise/GitDemo2.java
@@ -11,5 +11,7 @@ package com.kasi.exercise;
 public class GitDemo2 {
     public static void main(String[] args) {
         System.out.println("第一次提交");
+        System.out.println("第二次提交");
+        System.out.println("第三次提交");//第四次提交
     }
 }
