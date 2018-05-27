cb9eee449378fc991dc47de9fe4867b9bac53db4
kasi <ht333333@sina.com>
2018-05-27 14:15:37 +0800

    第一次提交

---
+++ git/src/java/com/kasi/exercise/GitDemo3.java
@@ -0,0 +1,15 @@
+package com.kasi.exercise;
+
+/**
+ * ${TODO}
+ *
+ * @Author: HuangTao
+ * @Date: 2018-05-27 14:15
+ * @Version:
+ */
+
+public class GitDemo3 {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
